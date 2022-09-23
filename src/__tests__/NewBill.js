f540ea076f79d3721ab47edc8c87a646105c89e2
joncoder <joncoder.kamite@gmail.com>
2022-09-23 19:31:54 +0200

    ajout des tests de fonctionnalité

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -18,40 +18,51 @@ import NewBill from "../containers/NewBill.js"
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
     // initialisation navigation
-    
-    test('I change the file', async () => {
-      const onNavigate = null
+    beforeAll(()=>{
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })   
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
       }))
       const html = NewBillUI()
       document.body.innerHTML = html
+    })
+
+    test('check image format', async () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
       const newbill = new NewBill({
         document, onNavigate, store, localStorage: window.localStorage
       })
       const file = new File(['billet-cartonne-sncf'], "billet-cartonne-sncf.jpg", {type: "image/png"})
-      const fileExt = file.name.split('.')[file.name.split('.').length -1]
-
       const authExtensions = ['jpg', 'jpeg', 'png']
+      const testedFile = screen.getByTestId("file")
       const handleChangeFile = jest.fn((e) => newbill.handleChangeFile(e))
-
+      testedFile.addEventListener('change', handleChangeFile)
+      userEvent.upload(testedFile, file)
+      const fileExt = testedFile.files[0].name.split('.')[testedFile.files[0].name.split('.').length -1]
+      expect(authExtensions).toContain(fileExt)
+    })
+    test('I change the file handleChangeFile should be called', async () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      const newbill = new NewBill({
+        document, onNavigate, store, localStorage: window.localStorage
+      })
+      const file = new File(['billet-cartonne-sncf'], "billet-cartonne-sncf.jpg", {type: "image/png"})
+      const handleChangeFile = jest.fn((e) => newbill.handleChangeFile(e))
       //const { getByTestId } = new render(<NewBillUI />);
-      const submit = screen.getByTestId('btn-send-bill')
+      //const submit = screen.getByTestId('btn-send-bill')
       const testedFile = screen.getByTestId("file")
       testedFile.addEventListener('change', handleChangeFile)
-      
       userEvent.upload(testedFile, file)
-      expect(testedFile.files[0]).toBe(file)
       expect(handleChangeFile).toHaveBeenCalled()
       /*if (authExtensions.includes(fileExt)) {
         expect(submit.hasAttribute('disabled')).toBe(false)
       }*/
     })
     test('Then onclick on submit should call handlesubmit', async () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //const onNavigate = null
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname })
       }
@@ -59,23 +70,10 @@ describe("Given I am connected as an employee", () => {
         document, onNavigate, store, localStorage: window.localStorage
       })
       //const { getByTestId } = new render(<NewBillUI />);
-      const submit = screen.getByTestId('btn-send-bill')
+      //const submit = screen.getByTestId('btn-send-bill')
       const form = screen.getByTestId('form-new-bill')
       const testedFile = screen.getByTestId("file")
       const file = new File(['billet-cartonne-sncf'], "billet-cartonne-sncf.xbm", {type: "image/bitmap"})
-      const bill = {
-        email: 'employee@test.tld',
-        type: screen.getByTestId("expense-type").value,
-        name:  screen.getByTestId("expense-name").value,
-        amount: parseInt(screen.getByTestId("amount").value),
-        date:  screen.getByTestId("datepicker").value,
-        vat: screen.getByTestId("vat").value,
-        pct: parseInt(screen.getByTestId("pct").value) || 20,
-        commentary: screen.getByTestId("commentary").value,
-        fileUrl: file.pathname,
-        fileName: file.name,
-        status: 'pending'
-      }
       const handlesubmit = jest.fn((e) => newbill.handleSubmit(e))
       const handleChangeFile = jest.fn((e) => newbill.handleChangeFile(e))
       //const updatebill = jest.fn((e) => newbill.updateBill(bill))
