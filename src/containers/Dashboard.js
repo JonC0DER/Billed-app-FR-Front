38e374f4c08e1f85d863b8fae81f4d4e138936bd
joncoder <joncoder.kamite@gmail.com>
2023-02-03 12:35:16 +0100

    correction de handleEditTiket

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -89,32 +89,24 @@ export default class {
   }
 
   handleEditTicket(e, bill, bills) {
-    if (this.counter === undefined || this.id !== bill.id || this.previousId !== this.id) this.counter = 0
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
-    ///////
-    if (this.confirmId !== this.id) this.previousId = this.confirmId
-    if (this.confirmId !== this.previousId && this.confirmId === this.id) this.counter = 1
-    else if (this.confirmId === this.previousId && this.confirmId !== this.id) this.counter = 0
-    else if (this.confirmId === this.id && this.previousId !== this.id) this.counter = 0
-    console.log(`counter : ${this.counter}\n conf : ${this.confirmId}\n id : ${this.id} \n previous id : ${this.previousId}` )
-    if (this.counter % 2 === 0) {
-      bills.forEach(b => {
-        $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
-      })
-      $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
-      $('.dashboard-right-container div').html(DashboardFormUI(bill))
-      $('.vertical-navbar').css({ height: '150vh' })
-      this.counter ++
-      this.confirmId = this.id
-    } else {
+   
+    if (!this.id) {
       $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
 
       $('.dashboard-right-container div').html(`
         <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
       `)
       $('.vertical-navbar').css({ height: '120vh' })
-      this.counter = 0
+    } else {
+      bills.forEach(b => {
+        $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
+      })
+      $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
+      $('.dashboard-right-container div').html(DashboardFormUI(bill))
+      $('.vertical-navbar').css({ height: '150vh' })
     }
+
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
     $('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill))
@@ -143,15 +135,15 @@ export default class {
   handleShowTickets(e, bills, index) {
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
+    
     if (this.counter % 2 === 0) {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
       $(`#status-bills-container${this.index}`)
-        .html(cards(filteredBills(bills, getStatus(this.index))))
+      .html(cards(filteredBills(bills, getStatus(this.index))))
       this.counter ++
     } else {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html("")
+      $(`#status-bills-container${this.index}`).html("")
       this.counter ++
     }
 
