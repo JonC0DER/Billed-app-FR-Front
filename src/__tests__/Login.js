d0c55b6c59e486a96d78fa87dca2b79e9b57887a
joncoder <joncoder.kamite@gmail.com>
2023-02-03 12:50:22 +0100

    mock admn conexion to dashbord

--- src/__tests__/Login.js
+++ src/__tests__/Login.js
@@ -4,23 +4,23 @@
 
 import LoginUI from "../views/LoginUI";
 import Login from "../containers/Login.js";
-import { ROUTES } from "../constants/routes";
+import { ROUTES, ROUTES_PATH } from "../constants/routes";
 import { fireEvent, screen } from "@testing-library/dom";
 
 describe("Given that I am a user on login page", () => {
   describe("When I do not fill fields and I click on employee button Login In", () => {
     test("Then It should renders Login page", () => {
       document.body.innerHTML = LoginUI();
-
+      
       const inputEmailUser = screen.getByTestId("employee-email-input");
       expect(inputEmailUser.value).toBe("");
-
+      
       const inputPasswordUser = screen.getByTestId("employee-password-input");
       expect(inputPasswordUser.value).toBe("");
 
       const form = screen.getByTestId("form-employee");
       const handleSubmit = jest.fn((e) => e.preventDefault());
-
+      
       form.addEventListener("submit", handleSubmit);
       fireEvent.submit(form);
       expect(screen.getByTestId("form-employee")).toBeTruthy();
@@ -38,10 +38,10 @@ describe("Given that I am a user on login page", () => {
       const inputPasswordUser = screen.getByTestId("employee-password-input");
       fireEvent.change(inputPasswordUser, { target: { value: "azerty" } });
       expect(inputPasswordUser.value).toBe("azerty");
-
+      
       const form = screen.getByTestId("form-employee");
       const handleSubmit = jest.fn((e) => e.preventDefault());
-
+      
       form.addEventListener("submit", handleSubmit);
       fireEvent.submit(form);
       expect(screen.getByTestId("form-employee")).toBeTruthy();
@@ -139,6 +139,14 @@ describe("Given that I am a user on login page", () => {
 
   describe("When I do fill fields in incorrect format and I click on admin button Login In", () => {
     test("Then it should renders Login page", () => {
+      const store = jest.fn();
+      
+      const inputData = {
+        type: "Admin",
+        email: "johdoe@email.com",
+        password: "zerty",
+        status: "connected",
+      };
       document.body.innerHTML = LoginUI();
 
       const inputEmailUser = screen.getByTestId("admin-email-input");
@@ -154,13 +162,14 @@ describe("Given that I am a user on login page", () => {
 
       form.addEventListener("submit", handleSubmit);
       fireEvent.submit(form);
+
       expect(screen.getByTestId("form-admin")).toBeTruthy();
     });
   });
-
+/*
   describe("When I do fill fields in correct format and I click on admin button Login In", () => {
     test("Then I should be identified as an HR admin in app", () => {
-      document.body.innerHTML = LoginUI();
+      document.body.innerHTML = LoginUI();      
       const inputData = {
         type: "Admin",
         email: "johndoe@email.com",
@@ -188,7 +197,7 @@ describe("Given that I am a user on login page", () => {
         },
         writable: true,
       });
-
+      
       // we have to mock navigation to test it
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname });
@@ -196,6 +205,12 @@ describe("Given that I am a user on login page", () => {
 
       let PREVIOUS_LOCATION = "";
 
+      const Store = {
+        login: jest.fn(() => Promise.resolve({ jwt: 'token' })),
+        users: jest.fn(() => ({
+          create: jest.fn(() => Promise.resolve({}))
+        }))
+      };     
       const store = jest.fn();
 
       const login = new Login({
@@ -207,24 +222,97 @@ describe("Given that I am a user on login page", () => {
       });
 
       const handleSubmit = jest.fn(login.handleSubmitAdmin);
+      //const handleSubmit = jest.fn(login.handleSubmitAdmin);
+      //login.handleSubmitAdmin = jest.fn()
       login.login = jest.fn().mockResolvedValue({});
       form.addEventListener("submit", handleSubmit);
       fireEvent.submit(form);
       expect(handleSubmit).toHaveBeenCalled();
+
       expect(window.localStorage.setItem).toHaveBeenCalled();
       expect(window.localStorage.setItem).toHaveBeenCalledWith(
         "user",
-        JSON.stringify({
-          type: "Admin",
-          email: inputData.email,
-          password: inputData.password,
-          status: "connected",
-        })
+        JSON.stringify(inputData)
       );
+      //expect(window.localStorage.setItem).toHaveBeenCalledWith('jwt', 'token')
     });
 
     test("It should renders HR dashboard page", () => {
       expect(screen.queryByText("Validations")).toBeTruthy();
+    });
+  });
+ */ 
+  
+  describe("When I do fill fields in correct format and I click on admin button Login In", () => {
+    test("Then I should be identified as an HR admin in app", () => {
+      const inputData = {
+        type: "Admin",
+        email: "johndoe@email.com",
+        password: "azerty",
+        status: "connected",
+      };
+      const formAdmin = {
+        addEventListener: jest.fn()
+      };
+      const localStorage = {
+        setItem: jest.fn()
+      };
+      const onNavigate = jest.fn();
+      const PREVIOUS_LOCATION = '';
+      const store = {
+        login: jest.fn(() => Promise.resolve({ jwt: 'token' })),
+        users: jest.fn(() => ({
+          create: jest.fn(() => Promise.resolve({}))
+        }))
+      };
+      const document = {
+        body: {
+          style: {
+            backgroundColor: '#000'
+          }
+        },
+        querySelector: jest.fn(() => formAdmin)
+      };
+
+      const login = new Login({ document, localStorage, onNavigate, PREVIOUS_LOCATION, store });
+      const e = {
+        preventDefault: jest.fn(),
+        target: {
+          querySelector: jest.fn((selector) => {
+            if (selector === `input[data-testid="admin-email-input"]`) {
+              return { value: 'admin@email.com' };
+            }
+            if (selector === `input[data-testid="admin-password-input"]`) {
+              return { value: 'password' };
+            }
+          })
+        }
+      };
+      
+      login.handleSubmitAdmin(e);
+      expect(e.preventDefault).toHaveBeenCalled();
+      expect(localStorage.setItem).toHaveBeenCalledWith('user', JSON.stringify({
+        type: inputData.type,
+        email: inputData.email,
+        password: inputData.password,
+        status: inputData.status
+      }));
+      expect(store.login).toHaveBeenCalledWith(JSON.stringify({
+        email: 'admin@email.com',
+        password: 'password'
+      }));
+      expect(localStorage.setItem).toHaveBeenCalledWith('jwt', 'token');
+      expect(store.users().create).toHaveBeenCalledWith({
+          data: JSON.stringify({
+          type: inputData.type,
+          email: inputData.email,
+          password: inputData.password,
+          status: inputData.status
+        })
+      });
+      expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH.Dashboard);
+      expect(document.body.style.backgroundColor).toBe('#fff');
+      expect(PREVIOUS_LOCATION).toBe(ROUTES_PATH.Dashboard);
     });
   });
 });
